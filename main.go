9dbc64fd35b361cdd496b4215770d9662fd25b9e
Lei Jing <lelvoo@outlook.com>
2019-01-16 12:06:52 +0800

    change the message text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World. this is a test for aws eks workshop ..."}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
